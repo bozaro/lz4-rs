21d05808ffd50db8415f1583a99bb8ee5e074d69
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-02-28 12:25:58 +0300

    Add mapping for native LZ4 functions

--- src/bin/version.rs
+++ src/bin/version.rs
@@ -1,8 +1,3 @@
-#![feature(core)]
-#![feature(io)]
-#![feature(path)]
-#![feature(os)]
-#![feature(std_misc)]
 extern crate "lz4-rs" as lz4;
 
 use lz4::liblz4::*;
--- src/liblz4.rs
+++ src/liblz4.rs
@@ -1,12 +1,39 @@
-// int LZ4_versionNumber(void)
-// int LZ4_compressBound(int isize)
-// LZ4_stream_t* LZ4_createStream(void)
-// int LZ4_compress_continue(LZ4_stream_t* LZ4_streamPtr, const char* source, char* dest, int inputSize)
-// int LZ4_freeStream(LZ4_stream_t* LZ4_streamPtr)
-// LZ4_streamDecode_t* LZ4_createStreamDecode(void)
-// int LZ4_decompress_safe_continue(LZ4_streamDecode_t* LZ4_streamDecode, const char* source, char* dest, int compressedSize, int maxDecompressedSize)
-// int LZ4_freeStreamDecode(LZ4_streamDecode_t* LZ4_stream)
+#[repr(C)]
+pub struct LZ4StreamEncode;
+
+#[repr(C)]
+pub struct LZ4StreamDecode;
 
 extern {
+	// int LZ4_versionNumber(void)
 	pub fn LZ4_versionNumber() -> i32;
+
+	// int LZ4_compressBound(int isize)
+	pub fn LZ4_compressBound(size: i32) -> i32;
+
+	// LZ4_stream_t* LZ4_createStream(void)
+	pub fn LZ4_createStream() -> *mut LZ4StreamEncode;
+
+	// int LZ4_compress_continue(LZ4_stream_t* LZ4_streamPtr, const char* source, char* dest, int inputSize)
+	pub fn LZ4_compress_continue(LZ4_stream: *mut LZ4StreamEncode, source: *const u8, dest: *mut u8, input_size: i32) -> i32;
+
+	// int LZ4_freeStream(LZ4_stream_t* LZ4_streamPtr)
+	pub fn LZ4_freeStream(LZ4_stream: *mut LZ4StreamEncode) -> i32;
+
+	// LZ4_streamDecode_t* LZ4_createStreamDecode(void)
+	pub fn LZ4_createStreamDecode() -> *mut LZ4StreamDecode;
+
+	// int LZ4_decompress_safe_continue(LZ4_streamDecode_t* LZ4_streamDecode, const char* source, char* dest, int compressedSize, int maxDecompressedSize)
+	pub fn LZ4_decompress_safe_continue(LZ4_stream: *mut LZ4StreamDecode, source: *const u8, dest: *mut u8, compressed_size: i32, max_decompressed_size: i32) -> i32;
+
+	// int LZ4_freeStreamDecode(LZ4_streamDecode_t* LZ4_stream)
+	pub fn LZ4_freeStreamDecode(LZ4_stream: *mut LZ4StreamDecode) -> i32;
+	
+}
+
+#[test]
+fn test_version_number() {
+	unsafe {
+		LZ4_versionNumber();
+	}
 }
