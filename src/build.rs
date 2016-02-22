d28406b63bf5a65352607cb4065df7d08db5c61c
TQ Hirsch <thequux@upstandinghackers.com>
2016-02-22 23:24:38 +0100

    Modified build script to *always* compile the C code with -O3 optimization

--- src/build.rs
+++ src/build.rs
@@ -1,10 +1,13 @@
 extern crate gcc;
 
 fn main() {
-	gcc::compile_library("liblz4.a", &[
-		"liblz4/lib/lz4.c",
-		"liblz4/lib/lz4frame.c",
-		"liblz4/lib/lz4hc.c",
-		"liblz4/lib/xxhash.c",
-	]);
+    let mut compiler = gcc::Config::new();
+    compiler
+        .file("liblz4/lib/lz4.c")
+        .file("liblz4/lib/lz4frame.c")
+        .file("liblz4/lib/lz4hc.c")
+        .file("liblz4/lib/xxhash.c")
+        // We always compile the C with optimization, because otherwise it is 20x slower.
+        .opt_level(3) ;
+    compiler.compile("liblz4.a");
 }
