64eaaaa8a4ff7f2655df84e30b7bce9c586777f7
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-10-28 15:46:56 -0400

    Don't panic in lz4-sys build script
    Print errors on stderr instead.

--- lz4-sys/build.rs
+++ lz4-sys/build.rs
@@ -1,9 +1,20 @@
 extern crate cc;
 
-use std::{env, fs};
+use std::{env, fs, process};
+use std::error::Error;
 use std::path::PathBuf;
 
 fn main() {
+    match run() {
+        Ok(()) => (),
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1);
+        }
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
     let mut compiler = cc::Build::new();
     compiler
         .file("liblz4/lib/lz4.c")
@@ -12,7 +23,9 @@ fn main() {
         .file("liblz4/lib/xxhash.c")
         // We always compile the C with optimization, because otherwise it is 20x slower.
         .opt_level(3);
-    match env::var("TARGET").unwrap().as_str()
+    match env::var("TARGET")
+        .map_err(|err| format!("reading TARGET environment variable: {}", err))?
+        .as_str()
     {
       "i686-pc-windows-gnu" => {
         compiler
@@ -22,15 +35,23 @@ fn main() {
     }
     compiler.compile("liblz4.a");
 
-    let src = env::current_dir().unwrap().join("liblz4").join("lib");
-    let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let src = env::current_dir()?.join("liblz4").join("lib");
+    let dst = PathBuf::from(env::var_os("OUT_DIR").ok_or("missing OUT_DIR environment variable")?);
     let include = dst.join("include");
-    fs::create_dir_all(&include).unwrap();
-    for e in fs::read_dir(&src).unwrap() {
-        let e = e.unwrap();
-        if e.file_name().into_string().unwrap().ends_with(".h") {
-            fs::copy(e.path(), include.join(e.file_name())).unwrap();
+    fs::create_dir_all(&include)
+        .map_err(|err| format!("creating directory {}: {}", include.display(), err))?;
+    for e in fs::read_dir(&src)? {
+        let e = e?;
+        let utf8_file_name = e.file_name().into_string()
+            .map_err(|_| format!("unable to convert file name {:?} to UTF-8", e.file_name()))?;
+        if utf8_file_name.ends_with(".h") {
+            let from = e.path();
+            let to = include.join(e.file_name());
+            fs::copy(&from, &to)
+                .map_err(|err| format!("copying {} to {}: {}", from.display(), to.display(), err))?;
         }
     }
     println!("cargo:root={}", dst.display());
+
+    Ok(())
 }
